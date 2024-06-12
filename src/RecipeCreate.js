be653a8350811ca53b746fac6abde91d9b9c254c
Thomas Sheehan <tsheehan@indeed.com>
2024-06-12 16:55:34 +0900

    Build RecipeCreate component and bug fix tests

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,86 @@
 import React, { useState } from "react";
 
-function RecipeCreate() {
+function RecipeCreate({ updateRecipes }) {
+  const initialData = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
 
-  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
-  // TODO: Add the required input and textarea form elements.
-  // TODO: Add the required submit and change handlers.
-  
+  const [formData, setFormData] = useState({ ...initialData });
+
+  const handleChange = ({ target }) => {
+    setFormData({ ...formData, [target.name]: target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateRecipes(formData);
+    setFormData({ ...initialData });
+  };
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <input
+                id="name"
+                name="name"
+                placeholder="name"
+                type="text"
+                onChange={handleChange}
+                required="true"
+                value={formData.name}
+              ></input>
+            </td>
+            <td>
+              <input
+                id="cuisine"
+                name="cuisine"
+                placeholder="cuisine"
+                type="text"
+                onChange={handleChange}
+                required="true"
+                value={formData.cuisine}
+              ></input>
+            </td>
+            <td>
+              <input
+                id="photo"
+                name="photo"
+                placeholder="URL"
+                type="text"
+                onChange={handleChange}
+                required="true"
+                value={formData.photo}
+              ></input>
+            </td>
+            <td>
+              <textarea
+                id="ingredients"
+                name="ingredients"
+                placeholder="ingredients"
+                rows="2"
+                onChange={handleChange}
+                required="true"
+                value={formData.ingredients}
+              ></textarea>
+            </td>
+            <td>
+              <textarea
+                id="preparation"
+                name="preparation"
+                placeholder="preparation"
+                rows="2"
+                onChange={handleChange}
+                required="true"
+                value={formData.preparation}
+              ></textarea>
+            </td>
             <td>
               <button type="submit">Create</button>
             </td>
--- src/RecipeView.js
+++ src/RecipeView.js
@@ -6,7 +6,7 @@ function RecipeView({ recipe, index, deleteRecipe }) {
       <td>{recipe.name}</td>
       <td>{recipe.cuisine}</td>
       <td>
-        <img alt={recipe.photo} />
+        <img alt={recipe.name} src={recipe.photo} />
       </td>
       <td className="content_td">{recipe.ingredients}</td>
       <td className="content_td">{recipe.preparation}</td>
