82ab125ce21d1d21a976bbbadc31d54e4dc17f17
Thomas Sheehan <tsheehan@indeed.com>
2024-06-12 16:11:21 +0900

    Create RecipeView component and link RecipeList component to main App component

---
+++ src/RecipeView.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function RecipeView({ recipe, index, deleteRecipe }) {
+  return (
+    <tr key={index}>
+      <td>{recipe.name}</td>
+      <td>{recipe.cuisine}</td>
+      <td>
+        <img alt={recipe.photo} />
+      </td>
+      <td className="content_td">{recipe.ingredients}</td>
+      <td className="content_td">{recipe.preparation}</td>
+      <td>
+        <button
+          name="delete"
+          onClick={() => {
+            deleteRecipe(index);
+          }}
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default RecipeView;
